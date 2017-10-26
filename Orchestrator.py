087d84abe8b34e239b4f770d9fad23478d94144d
ameet.chaubal <ameet.chaubal@accenture.com>
2017-10-26 14:58:02 -0400

    orchestrate

--- Orchestrator.py
+++ Orchestrator.py
@@ -1,7 +1,10 @@
 import logging
+import os
 import subprocess
-from subprocess import CalledProcessError
+import sys
 import time
+from subprocess import CalledProcessError
+
 import pandas as pd
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -12,40 +15,95 @@ PROJECT_COL = 'project name'
 STAGE_COL = 'stage'
 PARALLEL_COL = 'parallel'
 UTF = "utf-8"
+DATA = None
+WORKFLOW_ERROR_CODE = 5
+STAGES = None
+OUTPUT_MAX_LEN = 1024
 
-data = pd.read_csv(CONFIG_FILE, header=0)
+
+# To orchestrate multiple stages in defined sequence.
+# Assumption: a text file absolute path will be provided
+# this file contains 3 columns: project name,stage,command
+class Orchestrate:
+    workFlow = None
+    START = None
+
+    def __init__(self, workflow):
+        self.workFlow = workflow
+
+    def cleanseCommand(self, cmd):
+        args = cmd.strip().split()
+        LOGGER.debug("cmd args:%s", args)
+        return args
+
+    def init(self):
+        global START, DATA, STAGES
+        START = time.time()
+
+        try:
+            DATA = pd.read_csv(self.workFlow, header=0)
+        except:
+            LOGGER.error("Error reading workflow file:%s", sys.exc_info()[1])
+            sys.exit(WORKFLOW_ERROR_CODE)
+        STAGES = DATA.shape[0]
+        LOGGER.info("Start:-> Stages:%d", STAGES)
+
+    def finish(self):
+        global START
+        elapsed = time.time() - START
+        LOGGER.info("<-- Completed in:%.2f sec.", elapsed)
+
+    def process(self):
+        procs = []
+        for index, row in DATA.iterrows():
+            cmd = row[COMMAND_COL]
+            project = row[PROJECT_COL]
+            stage = row[STAGE_COL]
+            cmdArray = self.cleanseCommand(cmd)
+            LOGGER.info("Proj[%s]: stage:[%s] Cmdarray:%s", project, stage, cmdArray)
+            try:
+                proc = subprocess.Popen(cmdArray, stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+                ps = ProcessStruct(project, stage, proc, cmd)
+                procs.append(ps)
+
+            except OSError as o:
+                LOGGER.error("Err: oserror:%s", o.strerror)
+            except CalledProcessError as e:
+                LOGGER.debug("return:%s err:%s output:%s", e.returncode, e.stderr, e.output)
+            except ValueError as v:
+                LOGGER.error("Err: value:%s", v)
+
+        LOGGER.debug("Launch completed, will wait on completion: %d stages", len(procs))
+        for p in procs:
+            proc = p.proc
+            output, error = proc.communicate()
+            error = error.decode(UTF)
+            output = output.decode(UTF)
+            LOGGER.debug("\t%s {stage:%s, return:%s, output:\"%s\", error:\"%s\"}",
+                         p.name, p.stage, proc.returncode, output[:OUTPUT_MAX_LEN], error)
 
 
-def cleanseCommand(cmd):
-    args = cmd.strip().split()
-    LOGGER.debug("cmd args:%s", args)
-    return args
+class ProcessStruct:
+    name = None
+    stage = None
+    proc = None
+    cmd = None
 
-start=time.time()
-LOGGER.info("Start:->")
-for index, row in data.iterrows():
-    cmd = row[COMMAND_COL]
-    project = row[PROJECT_COL]
-    stage = row[STAGE_COL]
-    cmdArray = cleanseCommand(cmd)
-    LOGGER.info("Proj[%s]: stage:[%s] Cmdarray:%s", project, stage, cmdArray)
-    try:
-        proc = subprocess.Popen(cmdArray, stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
-        output, error = proc.communicate()
-        error = error.decode(UTF)
-        output = output.decode(UTF)
-        proc.wait()
-        LOGGER.debug("\tRETURNCODE:%s\n\t output:%s\n\t error:%s", proc.returncode, output, error)
+    def __init__(self, name, stage, proc, cmd):
+        self.name = name
+        self.stage = stage
+        self.proc = proc
+        self.cmd = cmd
 
-    except OSError as o:
-        print("OSERROR:{}".format(o))
-    except CalledProcessError as e:
-        out = e.output
-        print("output:{}".format(out))
-        print("code:{} err:{}".format(e.returncode, e.stderr))
-        # print("out---->{}".format(status))
-    except ValueError as v:
-        print("VALUE:{}".format(v))
 
-elapsed=time.time()-start
-LOGGER.info("<-- Completed in:%.2f sec.", elapsed)
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        LOGGER.error("Usage: %s <name of workflow file>", os.path.basename(sys.argv[0]))
+        sys.exit(1)
+
+    fileName = sys.argv[1]
+    LOGGER.info("WorkFlow:%s", fileName)
+    orch = Orchestrate(CONFIG_FILE)
+    orch.init()
+    orch.process()
+    orch.finish()
